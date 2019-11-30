21d5fb586f8a8d6e27c80b691348c53d214a7595
wise-integer-6834 <ghaxhaj@fordham.edu>
2019-11-30 22:04:17 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,18 @@ end
 
 introduction("Billy")
 
+def introduction_with_language(name,language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language("William","Python")
+
+def introduction_with_language_optional(name,language="Python")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Johnny")
+
 
 
 
